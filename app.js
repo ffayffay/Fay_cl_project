82180b8c993cafaa22a079bb1c87d3e19695b68a
Fay Marie <samsongreen72@gmail.com>
2018-06-17 11:12:02 -0400

    Addded new font also added javaScript to dynamically generate html for cake/cookie/pie flavors as well as the cake table

--- app.js
+++ app.js
@@ -58,11 +58,4 @@ function handler(event) {
 
 tabs.addEventListener('click', handler);
 
-// GIVES ALL <SPAN> ELEMENTS FONT-WEIGHT: BOLD
-let spanEle = document.querySelectorAll('span');
-console.log(spanEle);
-
-for(let i = 0; i < spanEle.length; i++) {
-	spanEle[i].style.fontWeight = 'bold';	
-}
 
